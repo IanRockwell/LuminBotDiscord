b8d77601348c969717f730a8c39cd4ece493adc4
Zabrid <ianrockwell10@gmail.com>
2023-12-21 01:26:54 -0500

    added notification in specified channel when a stream goes live

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,17 @@
 import discord
 import asyncio
-from data import data  # Assuming you have a data module
+import os
+from data import data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROMOTION_CHANNEL_ID = os.getenv("PROMOTION_CHANNEL_ID")
+
 
 class LuminBotDiscord(discord.Client):
     """
-    LuminBotDiscord class is a Discord bot built on the discord.py library.
-    It extends the discord.Client class to handle events and interact with the Discord API.
-
-    Attributes:
-        token (str): The bot token used for authentication with the Discord API.
-
-    Methods:
-        __init__(self, token): Initializes the LuminBotDiscord instance with the provided bot token.
-        on_ready(self): Event handler called when the bot has successfully connected to Discord.
-        on_message(self, message): Event handler called when a message is received in a channel.
-        change_bot_status(self): Updates the bot's presence with information about live streams.
-        run_bot(self): Initiates the bot's connection to the Discord API and starts event processing.
+    Main class for the discord bot.
     """
 
     def __init__(self, token):
@@ -49,7 +45,7 @@ class LuminBotDiscord(discord.Client):
         if message.author == self.user:
             return
 
-        #if message.content.startswith('$hello'):
+        # if message.content.startswith('$hello'):
         #    await message.channel.send('Hello!')
 
     async def change_bot_status(self):
@@ -58,6 +54,8 @@ class LuminBotDiscord(discord.Client):
         Periodically retrieves stream data and sets the bot's activity to display the number
         of live streams and total viewers. Runs in a loop with a 60-second interval.
         """
+        previous_stream_ids = set()
+
         def get_total_viewcount(doc):
             """
             Helper function to calculate the total viewer count from stream data.
@@ -76,12 +74,37 @@ class LuminBotDiscord(discord.Client):
             streams_data = data.get_data("streams")
             streams = streams_data["streams"]
 
+            current_stream_ids = {stream["id"] for stream in streams}
+
+            # Check for new streams
+            new_streams = current_stream_ids - previous_stream_ids
+            for stream_id in new_streams:
+                stream_info = next((stream for stream in streams if stream["id"] == stream_id), None)
+                if stream_info:
+                    await self.send_go_live_message(stream_info)
+
             total_viewer_count = get_total_viewcount(streams_data)
             live_channels = len(streams)
 
             status = discord.Activity(type=discord.ActivityType.watching,
                                       name=f"{live_channels} streams | {total_viewer_count} viewers")
             await self.change_presence(activity=status)
+
+            previous_stream_ids = current_stream_ids
             await asyncio.sleep(60)
+
+    async def send_go_live_message(self, stream_info):
+        """
+        Sends a message when a streamer goes live.
+
+        Parameters:
+            stream_info (dict): Information about the live stream.
+        """
+        channel_id = int(PROMOTION_CHANNEL_ID)
+        channel = self.get_channel(channel_id)
+
+        if channel:
+            await channel.send(f"{stream_info['user']['name']} has gone live using **LuminBot**! Watch here: https://twitch.tv/{stream_info['user']['name']}")
+
     def run_bot(self):
         self.run(self.token)
